fc71c5169c75ada43e8b5b054ef223d932fe903b
Марат Хабибуллин <khabibullin@hightech.today>
2017-03-07 09:33:06 +0300

    test

--- PivotTable/test.py
+++ PivotTable/test.py
@@ -6,17 +6,29 @@ import matplotlib.pyplot as plt
 
 PATH = exelDir = os.path.dirname(os.path.realpath(__file__))
 
+mainDataFile = PATH + "/sales-funnel.xlsx"
+
 # -----------------------------------------------------------
-df = pd.read_excel(PATH + "/sales-funnel.xlsx", "Sheet2")
-df.head()
+df = pd.read_excel(mainDataFile, "Sheet2")
 
 df.plot(kind='bar', x='Index', y='Price')
 plt.show()
 
 # -----------------------------------------------------------
+# Численность населения(городское\сельское)
+# -----------------------------------------------------------
 
-df = pd.read_excel(PATH + "/sales-funnel.xlsx", "Stat1")
-df.head()
+df = pd.read_excel(mainDataFile, "Stat1")
 
 df.plot(x='Year', y=['Count', 'City', 'Other'])
+plt.show()
+
+# -----------------------------------------------------------
+# Браки\разводы
+# -----------------------------------------------------------
+
+df = pd.read_excel(mainDataFile, "Brak")
+
+df.plot(x="Year", y=["Brak", "Razvod"])
+plt.grid(True)
 plt.show()
